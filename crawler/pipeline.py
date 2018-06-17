b2ef33d356a716349e49bec7fddd959ddbdc1cb4
tengshan2008 <tengshan2008@hotmail.com>
2018-06-18 00:41:06 +0800

    unset timeout

--- crawler/pipeline.py
+++ crawler/pipeline.py
@@ -55,7 +55,7 @@ def to_sql(title, novel_id, author, novel_type, content, date):
     conn.close()
 
 def to_api(title, novel_id, author, novel_type, content, date):
-    browser = robobrowser.RoboBrowser(history=True, timeout=60)
+    browser = robobrowser.RoboBrowser(history=True)
     url = 'http://apanr.net/'
     # open anyview a panel
     browser.open(url)
