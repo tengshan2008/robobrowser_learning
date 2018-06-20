2e5fac9f0e06651a6950f8826467395ae765f1c1
tengshan2008 <tengshan2008@hotmail.com>
2018-06-20 23:35:15 +0800

    reset if else struct

--- crawler/pipeline.py
+++ crawler/pipeline.py
@@ -48,7 +48,15 @@ def to_sql(title, novel_id, author, novel_type, content, date):
     param = (novel_id,)
     cursor.execute(query, param)
     if len(cursor.fetchall()) != 0:
-        if (cursor.fetchone() is None) or (len(cursor.fetchone()[0]) <= len(content)):
+        if cursor.fetchone() is None:
+            query = '''
+                update novel
+                set content = ?
+                where novel_id = ?
+            '''
+            param = (content, novel_id)
+            cursor.execute(query, param)
+        elif len(cursor.fetchone()[0]) < len(content):
             query = '''
                 update novel
                 set content = ?
