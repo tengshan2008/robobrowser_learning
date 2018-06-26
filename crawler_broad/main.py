2cd6ec492dab3dd5faa9afee00077a764e5f4277
tengshan2008 <tengshan2008@hotmail.com>
2018-06-27 00:20:09 +0800

    make init db

---
+++ crawler_broad/main.py
@@ -0,0 +1,187 @@
+import datetime
+import random
+import re
+import time
+
+import records
+import requests
+from robobrowser import RoboBrowser
+
+from pipeline import to_sql
+
+NEXT_PAGE = '下一頁'
+TODAY = '今天'
+YESTERDAY = '昨天'
+PATTERN = '草榴官方客戶端|來訪者必看的內容|发帖前必读|关于论坛的搜索功能|文学区违规举报专贴'
+R_START = 5
+R_END = 10
+
+host = 'https://cl.cbcb.us/'
+start_url = host + 'thread0806.php?fid=20'
+
+def is_end_page(browser):
+    """
+    lookup the end page
+    return bool
+    """
+    # print(browser.url)
+    # print(browser.find(class_='pages'))
+    if browser.find(class_='pages') is None:
+        return True
+    for label_a in browser.find_all('a'):
+        if label_a.string == NEXT_PAGE:
+            if label_a.get('href') == 'javascript:#':
+                return True
+    return False
+
+def next_page(browser):
+    """
+    get next page url
+    return url string
+    """
+    pages = browser.find(class_='pages')
+    for page in pages:
+        if page.string == NEXT_PAGE:
+            return page
+    return None
+
+def get_all_novels(url):
+    """
+    get all novel link
+    return links list [link1, link2, link3]
+    """
+    novels = list()
+    browser = RoboBrowser(history=True)
+    browser.open(url)
+    while not is_end_page(browser):
+        print(browser.url)
+        for tr in browser.select('tr.tr3.t_one.tac'):
+            if re.search(PATTERN, tr.h3.a.string) is None:
+                novels.append(tr)
+        time.sleep(5)
+        browser.follow_link(next_page(browser))
+    return novels
+
+def get_id(novel):
+    """
+    return novel's id for drop duplicates
+    """
+    href = novel.find('td', class_='tal').a['href']
+    novel_id = href.split('/')[-1].split('.')[0]
+    return novel_id
+
+def get_title(novel):
+    """
+    return novel's title
+    """
+    title = novel.find('td', class_='tal').a.string
+    return title
+
+def get_author(novel):
+    """
+    return novel's author
+    """
+    author = novel.find('a', class_='bl').string
+    return author
+
+def get_date(novel):
+    """
+    return novel's date
+    """
+    date = novel.find('div', class_='f12').string
+    if TODAY in date:
+        today = datetime.date.today()
+        return str(today)
+    if YESTERDAY in date:
+        yesterday = datetime.date.today() - datetime.timedelta(days=1)
+        return str(yesterday)
+    return date
+
+def get_type(novel):
+    """
+    return type string
+    """
+    novel_type = novel.find(class_='tal').contents[0].strip()
+    return novel_type
+
+def get_link(novel):
+    novel_link = novel.find('td', class_='tal').a
+    link = host + novel_link['href']
+    return link
+
+def get_content(novel, author):
+    """
+    get novel all content
+    return content string
+    """
+    browser = RoboBrowser(history=True)
+    novel_link = novel.find('td', class_='tal').a
+    link = host + novel_link['href']
+    time.sleep(random.randint(R_START, R_END))
+    # browser.follow_link(novel_link)
+    try:
+        browser.open(link)
+    except requests.exceptions.ConnectionError:
+        print('link failed', link)
+        return ''
+    else:
+        print('novel link', browser.url)
+    contents = list()
+    # look all page in a novel
+    while True:
+        content = get_cell_content(browser, author)
+        contents.append(content)
+        if is_end_page(browser):
+            break
+        time.sleep(random.randint(R_START, R_END))
+        next_page_link = next_page(browser)
+        if next_page_link is None:
+            continue
+        try:
+            browser.follow_link(next_page_link)
+        except requests.exceptions.ConnectionError:
+            print('link failed', browser.url)
+            continue
+        else:
+            print('page link', browser.url)
+    return "\n".join(contents)
+
+
+def get_cell_content(browser, author):
+    """
+    get novel cells
+    return [cell, cell, cell]
+    """
+    content = list()
+    cells = browser.find_all(class_='t t2')
+    for cell in cells:
+        if cell.find(class_='r_two').b.string != author:
+            continue
+        for cell_content in cell.find(class_=['tpc_content do_not_catch', 'tpc_content']).strings:
+            content.append(cell_content.strip())
+    return "\n".join(content)
+
+
+def run(url):
+    novels = get_all_novels(url)
+    i = 0
+    for novel in novels:
+        i = i + 1
+        print('have finished ', i)
+        novel_id = get_id(novel)
+        title = get_title(novel)
+        author = get_author(novel)
+        date = get_date(novel)
+        novel_type = get_type(novel)
+        novel_link = get_link(novel)
+        # content = get_content(novel, author)
+        to_sql(title, novel_id=novel_id, author=author, novel_type=novel_type, novel_link=novel_link, content='', date=date)
+
+def read():
+    db = records.Database('sqlite:///novel.db')
+    rows = db.query('select * from novel')
+    print(rows[0])
+
+if __name__ == "__main__":
+    # run(start_url)
+    read()
---
+++ crawler_broad/pipeline.py
@@ -0,0 +1,124 @@
+import os
+import sqlite3
+
+import robobrowser
+
+FATHER_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+BASE_DIR = os.path.normpath("%s/%s" % (FATHER_DIR, "data"))
+
+
+def output(title, novel_id=0, author="unkown", novel_type="unkown", content="unkown", date="unkown"):
+    pass
+    # to_sql(title, novel_id, author, novel_type, content, date)
+    # if match:
+    #     try:
+    #         to_api(title, novel_id, author, novel_type, content, date)
+    #     except Exception as e:
+    #         print(e)
+    #         print("novel title", title, "id", novel_id, " not upload")
+    #     else:
+    #         print("novel", title, "has upload")
+
+def to_sql(title, novel_id, author, novel_type, novel_link, content, date):
+    title = title.replace("'", "''")
+    author = author.replace("'", "''")
+    novel_type = novel_type.replace("'", "''")
+    novel_link = novel_link.replace("'", "''")
+    content = content.replace("'", "''")
+    date = date.replace("'", "''")
+
+    conn = sqlite3.connect('novel.db')
+    cursor = conn.cursor()
+    
+    cursor.execute(
+        '''
+            create table if not exists novel (
+            id integer primary key autoincrement,
+            novel_id int,
+            title text,
+            author text,
+            type text,
+            link text,
+            content text,
+            date text
+        )
+        '''
+    )
+
+    query = '''
+        select content from novel where novel_id = ?
+    '''
+    param = (novel_id,)
+    cursor.execute(query, param)
+    records = cursor.fetchall()
+    if len(records) != 0:
+        if len(records[0][0]) < len(content):
+            query = '''
+                update novel
+                set content = ?
+                where novel_id = ?
+            '''
+            param = (content, novel_id)
+            cursor.execute(query, param)
+            print('have new novel content, update it')
+        else:
+            print('the novel', title, 'has load in database')
+            cursor.close()
+            conn.commit()
+            conn.close()
+    query = '''
+        insert into novel
+        (novel_id, title, author, type, link, content, date)
+        values
+        (?, ?, ?, ?, ?, ?, ?)
+    '''
+    param = (novel_id, title, author, novel_type, novel_link, content, date)
+    cursor.execute(query, param)
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+def to_api(title, novel_id=0, author="unkown", novel_type="unkown", novel_link="unkown", content="unkown", date="unkown"):
+    browser = robobrowser.RoboBrowser(history=True)
+    url = 'http://apanr.net/'
+    # open anyview a panel
+    browser.open(url)
+    # login form
+    login_form = browser.get_form(id='log-in')
+    login_form['account'].value = 'tengshan2008'
+    login_form['password'].value = '8443658y'
+    browser.submit_form(login_form)
+    # access account
+    open_account = browser.find(href='/account')
+    browser.follow_link(open_account)
+    # upload form
+    upload_form = browser.get_form()
+    # add upload action field
+    upload_action_str = '\<input type="hidden" name="action" value="upload_file_post" \/\>'
+    upload_action = robobrowser.forms.fields.Input(upload_action_str)
+    upload_form.add_field(upload_action)
+    # add upload file field
+    # path = BASE_DIR + title + ".txt"
+    path = os.path.join(BASE_DIR, title + ".txt")
+    with open(path, 'w', encoding='utf-8') as f:
+        f.writelines([
+            "标题：" + title + "\n",
+            "作者：" + author + "\n",
+            "类型：" + novel_type + "\n",
+            "日期：" + date + "\n",
+            content + "\n"
+        ])
+    with open(path, 'r', encoding='utf-8') as f:
+        upload_form['file_to_upload'].value = f
+        browser.submit_form(upload_form)
+
+
+if __name__ == "__main__":
+    novel_id = 3183073
+    title = "测试3"
+    author = "傻瓜"
+    content = "一个晴朗的早晨，主人公死了。"
+    date = "2018-01-02 15:16:22"
+    output(title, author=author, novel_id=novel_id, content=content, date=date)
+    print(BASE_DIR)
+    print(os.path.join(BASE_DIR, title + ".txt"))
