3218d59891a674c08e93cda4871f6938d2be6403
tengshan2008 <tengshan2008@hotmail.com>
2018-07-03 23:32:26 +0800

    change page 17

--- crawler/run.py
+++ crawler/run.py
@@ -20,7 +20,7 @@ R_START = 5
 R_END = 10
 
 host = 'https://cl.cbcb.us/'
-start_url = host + 'thread0806.php?fid=20&search=&page=13'
+start_url = host + 'thread0806.php?fid=20&search=&page=17'
 
 def run(url):
     """
